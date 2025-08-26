dc7037c224ad0c0e4dcc82fe32b42e2b9e3b0251
ygb <517420999@qq.com>
2025-08-26 18:02:05 +0800

    完成列表点击查看

--- src/main/java/com/example/project/controller/NoteController.java
+++ src/main/java/com/example/project/controller/NoteController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/note")
@@ -21,6 +23,10 @@ public class NoteController {
     public Result getNotes(@Valid @PathVariable Long id){
 
         List list = noteService.getNotes(id);
-        return Result.ok(list);
+        List<Object> h = new ArrayList<>();
+        for(int i = 0; i < list.size(); i ++){
+            h.add(new Object[]{list.get(i), i});
+        }
+        return Result.ok(h);
     }
 }
