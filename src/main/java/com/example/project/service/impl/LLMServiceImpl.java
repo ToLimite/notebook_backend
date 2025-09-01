890b132edb7cdfa29f7d38c641363d6b935e2028
ygb <517420999@qq.com>
2025-09-01 17:32:00 +0800

    修正大模型api接口传文本含转义报错的bug

--- src/main/java/com/example/project/service/impl/LLMServiceImpl.java
+++ src/main/java/com/example/project/service/impl/LLMServiceImpl.java
@@ -14,11 +14,14 @@ public class LLMServiceImpl implements LLMService {
 
     @Override
     public String askLLM(String text) {
+        String t = text;
+        t = t.replaceAll("\\t", "");
+        t = t.replaceAll("\\n", "");
         HttpResponse<String> response = Unirest.post("https://open.bigmodel.cn/api/paas/v4/chat/completions")
                 .header("Authorization", "Bearer 036cb12c2c0841fd8869cbc07aa1d1dc.OyeNpytYJshBVQcX")
                 .header("Content-Type", "application/json")
                 .body("{\n  \"model\": \"glm-4-flashx-250414\",\n  \"do_sample\": true,\n  \"stream\": false,\n  \"thinking\": {\n    \"type\": \"enabled\"\n  },\n  \"temperature\": 0.6,\n  \"top_p\": 0.95,\n  \"response_format\": {\n    \"type\": \"text\"\n  },\n  \"messages\": [\n    {\n      \"role\": \"user\",\n      \"content\": \""
-                        + "请帮我总结一下这段笔记：" + text +
+                        + "请帮我总结一下这段笔记：" + t +
                         "\"\n    }\n  ]\n}")
                 .asString();
         String jsonString = response.getBody();
